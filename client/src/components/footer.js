915f0aef43cc49fededbcbe3325bd17d8ff369c2
Alec <31748545+burturt@users.noreply.github.com>
2023-02-06 14:51:04 -0800

    Update footer.js

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,7 +3,7 @@ import withStyles from './jss'
 const Footer = ({ classes }) => (
   <div class={classes.root}>
     <span>
-      Powered by <a href='https://rctf.redpwn.net/' target='_blank' rel='noopener noreferrer'>rCTF</a>
+      <a href='https://github.com/uclaacm/lactf-platform' target='_blank' rel='noopener noreferrer'>LA CTF Platform</a> - Powered by <a href='https://rctf.redpwn.net/' target='_blank' rel='noopener noreferrer'>rCTF</a>
       &nbsp;- Made with 🤍 by <a href='https://acmcyber.com/' target='_blank' rel='noopener noreferrer'>ACM Cyber</a>
       &nbsp;&amp; <a href='https://pbr.uclaacm.com/' target='_blank' rel='noopener noreferrer'>Psi Beta Rho</a>
     </span>
