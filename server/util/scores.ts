827b926f1648f65bfee796f04b0825a06d53e5b1
AVDestroyer <mr.arnav.vora@gmail.com>
2025-11-17 14:50:04 -0800

    update and improve point scoring curve

--- server/util/scores.ts
+++ server/util/scores.ts
@@ -1,19 +1,29 @@
 // rl is min challenge points
 // rh is max challenge points
-// maxSolves is usually 800 and honestly after that point it doesn't matter too much
+// maxSolves is usually 500-800 and honestly after that point it doesn't matter too much
 // solves is number of solves chall has
 //
 // formula im using (one curve for hard challs, one curve for easy challs:
-// min(max(428 * (0.995) ** solves + 75, 428 * (0.9978) ** solves, 100), 500)
+// min(max(rh * (0.96) ** (solves-1), rl + (rh*0.7 - rl) * (0.987) ** (solves-10), 100), 500)
 //
+// this formula lets you easily configure the max/min points such that
+// 1) For 0/1 solves, challenges are worth the max points
+// 2) For the first 10 solves, we use the hard curve which decays fast
+// 3) For the remainder of the solves, we use the easy curve which decays slowly. It bottoms out to the min points around the max solve count.
+
 // put below into desmos to see curve:
-// y_{2}=428\left(.99\right)^{x}+75
-// y_{3}=428\left(b\right)^{\left(x+a\right)}
-// b=0.9984
-// a=53
-// y=\min\left(\max\left(y_{2},y_{3},100\right),\ 500\right)
-export const getScore = (rl: number, rh: number, maxSolves: number, solves: number): number => {
-  const hardCurve = 428 * (0.99) ** solves + 76;
-  const easyCurve = 428 * (0.9984) ** (solves + 53);
+// h=500
+// l=100
+// f_{1}\left(x\right)=h\left(0.96\right)^{\left(x-1\right)}
+// f_{2}\left(x\right)=l\ +\ \left(h\cdot0.7\ -\ l\right)\left(0.987\right)^{\left(x-10\right)}
+// f\left(x\right)\ =\ \min\left(\max\left(f_{1}\left(x\right),f_{2}\left(x\right),l\right),h\right)
+export const getScore = (
+  rl: number,
+  rh: number,
+  maxSolves: number,
+  solves: number
+): number => {
+  const hardCurve = rh * 0.96 ** (solves - 1);
+  const easyCurve = rl + (rh * 0.7 - rl) * 0.987 ** (solves - 10);
   return Math.round(Math.min(Math.max(hardCurve, easyCurve, rl), rh));
-}
+};
